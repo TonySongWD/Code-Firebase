8c523e787cf891fb5c81037e5e5b2bb5b84a6e68
TonySong <tonysong072@gmail.com>
2018-12-18 14:32:31 +0800

    Init project.

---
+++ Code-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code-Firebase
+//
+//  Created by Tony Song on 12/18/18.
+//  Copyright © 2018 Tony Song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Code-FirebaseTests/Code_FirebaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  Code_FirebaseTests.swift
+//  Code-FirebaseTests
+//
+//  Created by Tony Song on 12/18/18.
+//  Copyright © 2018 Tony Song. All rights reserved.
+//
+
+import XCTest
+@testable import Code_Firebase
+
+class Code_FirebaseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
